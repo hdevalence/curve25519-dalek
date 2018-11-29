ee2281df26bc0d055089aafb48693a56f46acf63
Henry de Valence <hdevalence@hdevalence.ca>
2018-11-29 11:16:21 -0800

    fixup! don't generate unused constants in build.rs

--- build.rs
+++ build.rs
@@ -68,9 +68,7 @@ mod prelude;
 #[path = "src/window.rs"]
 mod window;
 
-use backend::serial::curve_models::AffineNielsPoint;
 use edwards::EdwardsBasepointTable;
-use window::NafLookupTable8;
 
 fn main() {
     // Enable the "stage2_build" feature in the main build stage
@@ -97,7 +95,6 @@ use edwards::EdwardsBasepointTable;
 use backend::serial::curve_models::AffineNielsPoint;
 
 use window::LookupTable;
-use window::NafLookupTable8;
 
 /// Table containing precomputed multiples of the Ed25519 basepoint \\\\(B = (x, 4/5)\\\\).
 pub const ED25519_BASEPOINT_TABLE: EdwardsBasepointTable = ED25519_BASEPOINT_TABLE_INNER_DOC_HIDDEN;
@@ -119,12 +116,16 @@ pub const ED25519_BASEPOINT_TABLE_INNER_DOC_HIDDEN: EdwardsBasepointTable = {:?}
         any(target_feature = "avx2", target_feature = "avx512ifma")
     )))]
     {
+        use backend::serial::curve_models::AffineNielsPoint;
+        use window::NafLookupTable8;
+
         let B = &constants::ED25519_BASEPOINT_POINT;
         let odd_multiples = NafLookupTable8::<AffineNielsPoint>::from(B);
 
         f.write_all(
             format!(
                 "\n
+use window::NafLookupTable8;
 /// Odd multiples of the basepoint `[B, 3B, 5B, 7B, 9B, 11B, 13B, 15B, ..., 127B]`.
 pub(crate) const AFFINE_ODD_MULTIPLES_OF_BASEPOINT: NafLookupTable8<AffineNielsPoint> = {:?};
 \n\n",
